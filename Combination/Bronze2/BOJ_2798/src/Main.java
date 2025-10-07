6c0957c5db2eebbb7d1d7cf94d90a1bc76a77102
me_nyong <gjalsdud1030@naver.com>
2025-10-07 15:00:31 +0900

    [BOJ_2798]: 블랙잭

---
+++ Combination/Bronze2/BOJ_2798/src/Main.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    static int N;
+    static int M;
+    static int[] card;
+    static int sum;
+    static int result = Integer.MIN_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        card = new int[N];
+        sum = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            card[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int j = 0; j < N; j++) {
+            combination(j, 3);
+        }
+
+        System.out.println(result);
+    }
+
+    private static void combination(int start, int depth) {
+        if (depth == 0) { //3개 다 뽑았다면 합을 구함
+            if (sum <= M) {
+                result = Math.max(result, sum);
+                return;
+            }
+        }
+
+        if (sum > M) return;  //가지치기 안해줘서 시간초과 남.
+
+        for (int i = start; i < N; i++) {
+            sum += card[i];
+            combination(i + 1, depth - 1);
+            sum -= card[i];
+        }
+    }
+}
