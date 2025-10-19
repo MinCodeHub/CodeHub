cecc818953e8f6813412b9441e4b7be4ce24fce4
me_nyong <gjalsdud1030@naver.com>
2025-10-19 15:15:21 +0900

    [BOJ_2839]: 설탕 배달

--- Greedy/Silver4/BOJ_2839/src/Main.java
+++ Greedy/Silver4/BOJ_2839/src/Main.java
@@ -2,29 +2,40 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static int N;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         N = Integer.parseInt(br.readLine());
-        int count = 0;
+        int cnt = 0;
+        while (N > 0) {
+            if (N % 5 == 0) {
+                //3의 배수일 때
+                N = N - 5;
+                cnt++;
+            } else if (N % 3 == 0) {
+                //5의 배수일 때
+                N = N - 3;
+                cnt++;
+            } else {
+                if (N >= 5) {
+                    N -= 5;
+                    cnt++;
+                } else {
+                    N -= 3;
+                    cnt++;
+                }
 
-        while(N > 0){
-            if(N % 5 == 0){   //N이 5의 배수라면
-                count += (N / 5);
-                N = 0;
-            }else{
-                N = N-3; //3을 뺀다
-                count +=1;
             }
         }
-        if(N < 0){
-            System.out.println(-1);
-        }else{
-            System.out.println(count);
 
+        if (N < 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(cnt);
         }
 
     }
