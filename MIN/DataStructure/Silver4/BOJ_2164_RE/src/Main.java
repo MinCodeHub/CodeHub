caccfde80ab31e7a7f51cac66d158ec8dd3b397d
me_nyong <gjalsdud1030@naver.com>
2025-11-13 15:32:57 +0900

    [BOJ_2164]: 카드 2

---
+++ MIN/DataStructure/Silver4/BOJ_2164_RE/src/Main.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            q.offer(i);
+        }
+
+        while (q.size() > 1) {
+            q.poll();
+            int c = q.poll();
+            q.offer(c);
+
+        }
+        System.out.println(q.poll());
+
+
+    }
+}
