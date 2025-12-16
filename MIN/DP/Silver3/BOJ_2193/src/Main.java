2133dfa0f065bbd75b8a619a0364498911a48403
me_nyong <gjalsdud1030@naver.com>
2025-12-16 22:19:38 +0900

    [BOJ_2193]: 이친수

---
+++ MIN/DP/Silver3/BOJ_2193/src/Main.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        long[] dp = new long[N + 1];
+
+        dp[1] = 1;
+        if (N >= 2) dp[2] = 1;
+
+        for (int i = 3; i <= N; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        System.out.println(dp[N]);
+
+    }
+}
